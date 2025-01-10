1ee3564b93555822434b3764ccae5822dd431a2a
akash ahmed <hafejmohammadakash@gmail..com>
2025-01-10 19:43:34 +0600

    done

--- problemt.js
+++ problemt.js
@@ -35,20 +35,93 @@ function sendNotification(email) {
 
 // // _______________ problem 03 __________________________________________/
 
-/*function signature/sample */
-function checkDigitsInName(name) {
-      if (typeof name !== "string") {
-            return "invalid input"
+// /*function signature/sample */
+// function checkDigitsInName(name) {
+//       if (typeof name !== "string") {
+//             return "invalid input"
+//       }
+
+//       for (let char of name) {
+//             if (!isNaN(char) && char !== " ") {
+//                   return true;
+//             }
+//       }
+//       return false;
+// }
+// // console.log(checkDigitsInName(["Raj"]))
+
+
+
+
+// function calculateFinalScore(obj) {
+//       if (
+//             typeof obj !== "object" ||
+//             Array.isArray(obj) ||
+//             obj === null
+//       ) {
+//             return "Invalid Input";
+//       }
+
+//       const { name, testScore, schoolGrade, isFFamily } = obj;
+
+//       // Validate the object properties
+//       if (
+//             typeof name !== "string" ||
+//             typeof testScore !== "number" || testScore > 50 ||
+//             typeof schoolGrade !== "number" || schoolGrade > 30 ||
+//             typeof isFFamily !== "boolean"
+//       ) {
+//             return "Invalid Input";
+//       }
+
+
+//       let finalScore = testScore + schoolGrade;
+//       if (isFFamily) {
+//             finalScore += 20; 
+//       }
+
+//       return finalScore >= 80;
+// }
+
+
+// console.log(calculateFinalScore({ name: "Rajib", testScore: 45, schoolGrade: 25, isFFamily: true }));
+// console.log(calculateFinalScore({ name: "Rajib", testScore: 45, schoolGrade: 25, isFFamily: false }));
+// console.log(calculateFinalScore("hello")); 
+// console.log(calculateFinalScore({ name: "Rajib", testScore: 15, schoolGrade: 25, isFFamily: true })); 
+
+
+
+function calculateFinalScore(obj) {
+      if (
+            typeof obj !== "object" ||
+            Array.isArray(obj) ||
+            obj === null
+      ) {
+            return "Invalid Input";
       }
 
-    for (let char of name) {
-      if (!isNaN(char) && char !== " ") { 
-          return true;
+
+      const { name, testScore, schoolGrade, isFFamily } = obj;
+
+      if(
+            typeof name !== "string" ||
+            typeof testScore !== "number" || schoolGrade > 50 ||
+            typeof schoolGrade !== "number" || schoolGrade > 30 ||
+            typeof isFFamily !== "boolean"
+      ){
+            return "Invalid Input";
       }
-  }
-  return false;
+
+      let final = testScore + schoolGrade;
+      if(isFFamily === true){
+            final = final + 20;
+      }
+      return final >= 80;
+
 }
-console.log(checkDigitsInName(["Raj"]))
 
 
-
+console.log(calculateFinalScore({ name: "raib", testScore: 45, schoolGrade: 25, isFFamily: true }));
+console.log(calculateFinalScore( { name: "Rajib", testScore: 45,  schoolGrade: 25, isFFamily : false}));
+console.log(calculateFinalScore("hello"));
+console.log(calculateFinalScore({ name: "Rajib", testScore: 15,  schoolGrade: 25, isFFamily : true  }));
