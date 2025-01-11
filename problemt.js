ea534fe35817f1c28a4223304364e139babefc87
akash ahmed <hafejmohammadakash@gmail..com>
2025-01-11 12:32:00 +0600

    done here

--- problemt.js
+++ problemt.js
@@ -103,25 +103,49 @@ function calculateFinalScore(obj) {
 
       const { name, testScore, schoolGrade, isFFamily } = obj;
 
-      if(
+      if (
             typeof name !== "string" ||
             typeof testScore !== "number" || schoolGrade > 50 ||
             typeof schoolGrade !== "number" || schoolGrade > 30 ||
             typeof isFFamily !== "boolean"
-      ){
+      ) {
             return "Invalid Input";
       }
 
       let final = testScore + schoolGrade;
-      if(isFFamily === true){
+      if (isFFamily === true) {
             final = final + 20;
       }
       return final >= 80;
 
 }
+// console.log(calculateFinalScore({ name: "raib", testScore: 45, schoolGrade: 25, isFFamily: true }));
+// console.log(calculateFinalScore( { name: "Rajib", testScore: 45,  schoolGrade: 25, isFFamily : false}));
+// console.log(calculateFinalScore("hello"));
+// console.log(calculateFinalScore({ name: "Rajib", testScore: 15,  schoolGrade: 25, isFFamily : true  }));
 
 
-console.log(calculateFinalScore({ name: "raib", testScore: 45, schoolGrade: 25, isFFamily: true }));
-console.log(calculateFinalScore( { name: "Rajib", testScore: 45,  schoolGrade: 25, isFFamily : false}));
-console.log(calculateFinalScore("hello"));
-console.log(calculateFinalScore({ name: "Rajib", testScore: 15,  schoolGrade: 25, isFFamily : true  }));
+
+function waitingTime(waitingTimes, serialNumber) {
+      if (!Array.isArray(waitingTimes) || typeof serialNumber !== 'number') {
+            return 'Invalid Input';
+      }
+
+      const totalTime = waitingTimes.reduce((sum, time) => sum + time, 0);
+
+      const averageTime = Math.round(totalTime / waitingTimes.length);
+
+      const remainingPeople = serialNumber - waitingTimes.length - 1;
+
+      const waitingTime = remainingPeople > 0 ? remainingPeople * averageTime : 0;
+
+      return waitingTime;
+}
+
+console.log(waitingTime([3, 5, 7, 11, 6], 10));  // Output: 24
+console.log(waitingTime([13, 2], 6));  // Output: 24
+console.log(waitingTime([13, 2, 6, 7, 10], 6));  // Output: 0
+console.log(waitingTime([6], 4));  // Output: 12
+console.log(waitingTime(7, 10));  // Output: Invalid Input
+console.log(waitingTime("[6,2]", 9));  // Output: Invalid Input
+console.log(waitingTime([7, 8, 3, 4, 5], "9"));  // Output: Invalid Input
